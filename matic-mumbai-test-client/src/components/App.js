07e32679b4c93887cad771701bfc08e3736af9d7
DoubleHelix <tjrnyc5@gmail.com>
2021-06-23 14:02:24 -0400

    install and config for material ui

--- matic-mumbai-test-client/src/components/App.js
+++ matic-mumbai-test-client/src/components/App.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import styled from "styled-components/macro";
+import swag_logo_2 from "../images/swag_logo_2.jpg";
 
 
 const S = {};
 S.AppContainer = styled.div`
-  border: 2px yellow solid;
+  border: 2px red solid;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`;
+
+S.LoaderImage = styled.div`
+  position: relative;
+  margin: auto;
+  background-size: cover;
+  background-position: center;
+  box-shadow: 0px 4px 13px 0px rgba(0, 0, 0, 0.1);
+  background-image: ${swag_logo_2};
+  border: 2px blue solid;
 `;
 
 
 const App = () => {
   return (
     <S.AppContainer>
-      <p> Hello World! ! !</p>
+     <S.LoaderImage/>
     </S.AppContainer>
   );
 }
