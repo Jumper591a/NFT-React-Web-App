c83ec2eebb3ccc777ae34c15b3c56411d0343e48
DoubleHelix <tjrnyc5@gmail.com>
2021-06-24 00:24:02 -0400

    completed load banner and connect wallet design

--- matic-mumbai-test-client/src/components/App.js
+++ matic-mumbai-test-client/src/components/App.js
@@ -1,33 +1,243 @@
-import React from 'react';
-import styled from "styled-components/macro";
-import swag_logo_2 from "../images/swag_logo_2.jpg";
+import React, { useState, useEffect, useRef } from "react";
 
+import styled, { css } from "styled-components/macro";
+import swag_logo_2 from "../images/swag_logo_2.jpg";
+import swag_logo_1 from "../images/swag_logo_1.png";
+import Button from "@material-ui/core/Button";
+import Tooltip from "@material-ui/core/Tooltip";
+import VpnKeyOutlinedIcon from "@material-ui/icons/VpnKeyOutlined";
+import Avatar from "@material-ui/core/Avatar";
+import VpnKeyTwoToneIcon from "@material-ui/icons/VpnKeyTwoTone";
+import VpnKeyRoundedIcon from "@material-ui/icons/VpnKeyRounded";
+import "../index.css";
+
+import {
+  jello,
+  flash,
+  rotateInFadeOutUp,
+  rotateIn,
+  rotateOut,
+  fadeOutUp,
+  fadeIn,
+  fadeOutUp2,
+  slideLeft,
+  spin,
+} from "../shared/_Keyframes";
 
 const S = {};
 S.AppContainer = styled.div`
-  border: 2px red solid;
+  /* border: 2px red solid; */
   display: flex;
   flex-direction: column;
-  height: 100vh;
-`;
-
-S.LoaderImage = styled.div`
   position: relative;
-  margin: auto;
-  background-size: cover;
+  height: 100vh;
+  background-color: #f2f3f3;
+`;
+S.LoaderContainer = styled.div`
+  display: inherit;
+  flex-direction: inherit;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: #ececec;
+  /* margin: 5% 0; */
+  animation-name: ${fadeOutUp};
+  animation-delay: 0.9s;
+  transform-origin: center;
+  animation-duration: 1.5s;
+  animation-timing-function: ease-in-out;
+  animation-fill-mode: forwards;
+  /* box-shadow: 0px 4px 13px 0px rgba(255, 255, 255, 0.); */
+  /* z-index: -15; */
+  /* border: 2px green solid; */
+`;
+S.LoaderImage = styled.div`
+  position: inherit;
+  background-size: contain;
+  background-repeat: no-repeat;
   background-position: center;
-  box-shadow: 0px 4px 13px 0px rgba(0, 0, 0, 0.1);
-  background-image: ${swag_logo_2};
-  border: 2px blue solid;
+  background-image: url(${swag_logo_2});
+  width: 70vw;
+  height: 70vh;
+  margin: 15% 10% 10% 15%;
+  animation-name: ${rotateIn};
+  transform-origin: center;
+  animation-duration: 0.9s;
+  animation-timing-function: ease-in-out;
+  animation-fill-mode: forwards;
+
+  /* border: 2px blue solid; */
+`;
+S.Header = styled.div`
+  display: flex;
+  position: relative;
+  justify-content: center;
+  align-items: center;
+  background-color: #333333;
+  box-shadow: 0px 4px 13px 0px rgba(0, 0, 0, 0.4);
+  border-bottom-left-radius: 5px;
+  border-bottom-right-radius: 5px;
+`;
+S.HeaderLogo = styled.img`
+  padding: 20px;
+  width: 400px;
+  object-fit: contain;
+
+  /* background-color: #333333; */
 `;
 
+S.Button = styled(Button)`
+  background-color: white !important;
+  border: 2px solid #440099 !important;
+  color: #440099 !important;
+  font-weight: bold !important;
+  /* position: absolute; */
+  margin: 0 !important;
+  right: -10vw;
+  display: ${(props) => props.display};
+  animation-name: ${(props) =>
+    props.animation === "slideLeft" ? slideLeft : ""};
+  transform-origin: center;
+  animation-duration: 1.2s;
+  animation-timing-function: ease-in-out;
+  animation-iteration-count: 1;
+  animation-fill-mode: forwards;
+  /* font-family: "Viaoda Libre", cursive !important; */
+  /* align-self: flex-end;
+  justify-self: flex-end; */
+`;
+S.Tooltip = styled(Tooltip)``;
+
+S.ExmapleText = styled.p`
+  border: 2px purple solid;
+`;
+
+S.WalletConnect = styled(Button)`
+  width: 180px;
+`;
+
+S.WalletConnectAvatar = styled(Avatar)`
+  vertical-align: center;
+  background-color: #fc3290 !important;
+  margin: 0 10px 0 20px;
+  align-self: center;
+  /* position: none !important; */
+  /* display: none !important; */
+  z-index: -1;
+  width: 100px !important;
+  height: 100px !important;
+  border: 2px #f26d79 groove;
+  cursor: pointer;
+  animation-name: ${(props) => (props.animation === "fadeIn" ? fadeIn : "")};
+  transform-origin: center;
+  animation-duration: 3.2s;
+  animation-timing-function: ease-in-out;
+  animation-iteration-count: 1;
+  animation-fill-mode: forwards;
+  animation-delay: 0.8s;
+
+  &:hover > svg {
+    animation-name: ${jello};
+    transform-origin: center;
+    animation-duration: 1s;
+    animation-timing-function: ease-in-out;
+  }
+  &:focus {
+    z-index: 0;
+    animation-name: ${rotateOut};
+    transform-origin: center;
+    animation-duration: 1s;
+    animation-timing-function: ease-in-out;
+    animation-fill-mode: forwards;
+    animation-delay: 0s;
+  }
+
+  box-shadow: 0px 4px 13px 0px rgba(0, 0, 0, 0.1);
+`;
+
+S.WalletConnectContain = styled.span`
+  display: flex;
+  justify-content: center;
+  margin-top: 30vh;
+`;
+
+S.VpnKeyTwoToneIcon = styled(VpnKeyRoundedIcon)`
+  /* position: relative !important; */
+  width: 50px !important;
+  height: 50px !important;
+  color: #fff;
+`;
+
+S.WalletConnectText = styled.p`
+  align-self: center;
+  font-size: 64px;
+  font-family: "Sacramento", cursive;
+  color: #fc3290;
+  animation-name: ${(props) =>
+    props.animation === "fadeOutUp2" ? fadeOutUp2 : ""};
+  transform-origin: center;
+  animation-duration: 1.2s;
+  animation-timing-function: ease-in-out;
+  animation-iteration-count: 1;
+  animation-fill-mode: forwards;
+  animation-delay: 0.2s;
+`;
 
 const App = () => {
+  const [connected, setConnected] = useState(0);
+  const [open, setOpen] = React.useState(false);
+
+  const handleTooltipClose = () => {
+    setOpen(false);
+  };
+
+  const handleTooltipOpen = () => {
+    setOpen(true);
+  };
+
+  const connectWallet = (e) => {
+    e.preventDefault();
+    setConnected(1);
+  };
+
   return (
     <S.AppContainer>
-     <S.LoaderImage/>
+      <S.LoaderContainer>
+        <S.LoaderImage />
+      </S.LoaderContainer>
+      <S.Header>
+        <S.HeaderLogo src={swag_logo_1} />
+        <S.Tooltip
+          disableFocusListener
+          disableTouchListener
+          title="Account: 0xd53CDbdD044bc9d58F6043ff7868A546A9FD2700 "
+        >
+          <S.Button
+            animation={connected ? "slideLeft" : ""}
+            disableRipple
+            display={connected ? "inline!important" : "none!important"}
+          >
+            Connected
+          </S.Button>
+        </S.Tooltip>
+      </S.Header>
+      <S.WalletConnectContain>
+        <S.WalletConnectText animation={connected ? "fadeOutUp2" : ""}>
+          Connect
+        </S.WalletConnectText>
+        <S.WalletConnectAvatar
+          animation={!connected ? "fadeIn" : ""}
+          onClick={(event) => connectWallet(event)}
+          tabIndex="1"
+        >
+          <S.VpnKeyTwoToneIcon />
+        </S.WalletConnectAvatar>
+        <S.WalletConnectText animation={connected ? "fadeOutUp2" : ""}>
+          Wallet
+        </S.WalletConnectText>
+      </S.WalletConnectContain>
     </S.AppContainer>
   );
-}
+};
 
 export default App;
---
+++ matic-mumbai-test-client/src/shared/_Keyframes.js
@@ -0,0 +1,559 @@
+import { keyframes } from "styled-components";
+
+export const slideInTop = keyframes`
+  0% {
+    opacity: 0;
+    transform: translateY(100px);
+  }
+  20% {
+    opacity: 1;
+    transform: translateY(0px);
+  }
+  85% {
+    opacity: 1;
+    transform: translateY(0px);
+  }
+  100% {
+    opacity: 0;
+    transform: translateY(-10px);
+  }
+`;
+
+export const spin = keyframes`
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  `;
+
+export const slightRotate = keyframes`
+    0% {
+      -webkit-transform: rotate(0deg);
+    }
+    33% {
+      -webkit-transform: rotate(15deg);
+    }
+    66% {
+      -webkit-transform: rotate(-15deg);
+    }
+    99% {
+      -webkit-transform: rotate(0deg);
+    }
+  `;
+
+export const animationFrames = keyframes`
+    0% {
+            transform: scale(1.1);
+
+    }
+    100% {
+      transform: translate(-50px, -50px);
+    }
+    `;
+
+export const fadeAway = keyframes`
+   0% {
+      opacity 1;
+      }
+   100% {
+    opacity 0;
+   }
+   `;
+
+export const scaleUp = keyframes`
+0% {
+  transform: scale(1.0);
+
+}
+100% {
+  transform: scale(1.2);
+
+}
+`;
+export const scaleDown = keyframes`
+0% {
+  transform: scale(1.2);
+
+}
+100% {
+  transform: scale(1.0);
+
+}
+`;
+
+export const heartBeat = keyframes`
+    0% {
+      transform: scale(1);
+    }
+
+    14% {
+      transform: scale(1.3);
+    }
+
+    28% {
+      transform: scale(1);
+    }
+
+    42% {
+      transform: scale(1.3);
+    }
+
+    70% {
+      transform: scale(1);
+    }
+    `;
+
+export const bounce = keyframes`
+    from,
+    20%,
+    53%,
+    to {
+      animation-timing-function: cubic-bezier(0.215, 0.61, 0.355, 1);
+      transform: translate3d(0, 0, 0);
+    }
+
+    40%,
+    43% {
+      animation-timing-function: cubic-bezier(0.755, 0.05, 0.855, 0.06);
+      transform: translate3d(0, -30px, 0) scaleY(1.1);
+    }
+
+    70% {
+      animation-timing-function: cubic-bezier(0.755, 0.05, 0.855, 0.06);
+      transform: translate3d(0, -15px, 0) scaleY(1.05);
+    }
+
+    80% {
+      transition-timing-function: cubic-bezier(0.215, 0.61, 0.355, 1);
+      transform: translate3d(0, 0, 0) scaleY(0.95);
+    }
+
+    90% {
+      transform: translate3d(0, -4px, 0) scaleY(1.02);
+    }
+    `;
+
+export const rubberBand = keyframes`
+    from {
+      transform: scale3d(1, 1, 1);
+    }
+  
+    30% {
+      transform: scale3d(1.25, 0.75, 1);
+    }
+  
+    40% {
+      transform: scale3d(0.75, 1.25, 1);
+    }
+  
+    50% {
+      transform: scale3d(1.15, 0.85, 1);
+    }
+  
+    65% {
+      transform: scale3d(0.95, 1.05, 1);
+    }
+  
+    75% {
+      transform: scale3d(1.05, 0.95, 1);
+    }
+  
+    to {
+      transform: scale3d(1, 1, 1);
+    }
+  `;
+
+export const jello = keyframes`
+    from,
+    11.1%,
+    to {
+      transform: translate3d(0, 0, 0);
+    }
+  
+    22.2% {
+      transform: skewX(-12.5deg) skewY(-12.5deg);
+    }
+  
+    33.3% {
+      transform: skewX(6.25deg) skewY(6.25deg);
+    }
+  
+    44.4% {
+      transform: skewX(-3.125deg) skewY(-3.125deg);
+    }
+  
+    55.5% {
+      transform: skewX(1.5625deg) skewY(1.5625deg);
+    }
+  
+    66.6% {
+      transform: skewX(-0.78125deg) skewY(-0.78125deg);
+    }
+  
+    77.7% {
+      transform: skewX(0.390625deg) skewY(0.390625deg);
+    }
+  
+    88.8% {
+      transform: skewX(-0.1953125deg) skewY(-0.1953125deg);
+    }
+  `;
+
+export const shakeY = keyframes`
+  from,
+  to {
+    transform: translate3d(0, 0, 0);
+  }
+
+  10%,
+  30%,
+  50%,
+  70%,
+  90% {
+    transform: translate3d(0, -3px, 0);
+  }
+
+  20%,
+  40%,
+  60%,
+  80% {
+    transform: translate3d(0, 3px, 0);
+  }
+`;
+
+export const hinge = keyframes`
+  0% {
+    animation-timing-function: ease-in-out;
+  }
+
+  20%,
+  60% {
+    transform: rotate3d(0, 0, 1, 80deg);
+    animation-timing-function: ease-in-out;
+  }
+
+  40%,
+  80% {
+    transform: rotate3d(0, 0, 1, 60deg);
+    animation-timing-function: ease-in-out;
+    opacity: 1;
+  }
+
+  to {
+    transform: translate3d(0, 700px, 0);
+    opacity: 0;
+  }
+`;
+
+export const shakeX = keyframes`
+  from,
+  to {
+    transform: translate3d(0, 0, 0);
+  }
+
+  10%,
+  30%,
+  50%,
+  70%,
+  90% {
+    transform: translate3d(-5px, 0, 0);
+  }
+
+  20%,
+  40%,
+  60%,
+  80% {
+    transform: translate3d(5px, 0, 0);
+  }
+`;
+
+export const swing = keyframes`
+  20% {
+    transform: rotate3d(0, 0, 1, 15deg);
+  }
+
+  40% {
+    transform: rotate3d(0, 0, 1, -10deg);
+  }
+
+  60% {
+    transform: rotate3d(0, 0, 1, 5deg);
+  }
+
+  80% {
+    transform: rotate3d(0, 0, 1, -5deg);
+  }
+
+  to {
+    transform: rotate3d(0, 0, 1, 0deg);
+  }
+}`;
+
+export const shake = keyframes`
+  0% { transform: translate(1px, 1px) rotate(0deg); }
+  10% { transform: translate(-1px, -2px) rotate(-1deg); }
+  20% { transform: translate(-3px, 0px) rotate(1deg); }
+  30% { transform: translate(3px, 2px) rotate(0deg); }
+  40% { transform: translate(1px, -1px) rotate(1deg); }
+  50% { transform: translate(-1px, 2px) rotate(-1deg); }
+  60% { transform: translate(-3px, 1px) rotate(0deg); }
+  70% { transform: translate(3px, 1px) rotate(-1deg); }
+  80% { transform: translate(-1px, -1px) rotate(1deg); }
+  90% { transform: translate(1px, 2px) rotate(0deg); }
+  100% { transform: translate(1px, -2px) rotate(-1deg); }
+`;
+
+export const enterUpExitRight = keyframes`
+0%{ 
+  display: block;
+  visibility: visible;
+  z-index: 1;
+  -webkit-transform: rotate(0deg);
+
+  transform: translateY(20px) scale(0.7);
+    opacity: 0.7;
+  }
+   20% {
+    transform: translateY(0px) scale(0.7);
+    opacity: 0.7;
+    -webkit-transform: rotate(15deg);
+  }
+
+30%{
+  -webkit-transform: rotate(0deg);
+
+}
+  40% {
+    -webkit-transform: rotate(15deg);
+
+    transform: scale(1);
+    opacity: 1;
+  }
+  50%{
+    -webkit-transform: rotate(-15deg);
+  }
+
+  60%{
+    -webkit-transform: rotate(15deg);
+
+    transform: translateX(0px) scale(0.7);
+    opacity: 0.7;
+  }
+  90%{
+    -webkit-transform: rotate(0deg);
+    transform: translateX(20px) scale(0.7);
+    opacity: 0.7;
+    visibility: hidden;
+    z-index: -5;
+  }
+
+`;
+
+export const enterRightExitUp = keyframes`
+0%{ 
+  display: block;
+  visibility: visible;
+  z-index: 1;
+  -webkit-transform: rotate(0deg);
+
+  transform: translateX(-20px) scale(0.7);
+    opacity: 0.7;
+  }
+   20% {
+    transform: translateX(0px) scale(0.7);
+    opacity: 0.7;
+    -webkit-transform: rotate(15deg);
+  }
+
+30%{
+  -webkit-transform: rotate(0deg);
+
+}
+  40% {
+    -webkit-transform: rotate(15deg);
+
+    transform: scale(1);
+    opacity: 1;
+  }
+  50%{
+    -webkit-transform: rotate(-15deg);
+  }
+
+  60%{
+    -webkit-transform: rotate(15deg);
+
+    transform: translateY(0px) scale(0.7);
+    opacity: 0.7;
+  }
+  90%{
+    -webkit-transform: rotate(0deg);
+    transform: translateY(-20px) scale(0.7);
+    opacity: 0.7;
+    visibility: hidden;
+    z-index: -5;
+  }
+
+`;
+
+export const exitUpEnterUp = keyframes`
+0%{ 
+  /* visibility: hidden; */
+  z-index: 1;
+  transform: translateY(0px) scale(0.7);
+  opacity: 0.7;
+  }
+   20% {
+  transform: translateY(-20px) scale(0.7);
+  opacity: 0;
+  z-index: -5;
+  visibility: hidden;
+  }
+
+80%{
+  transform: translateY(20px) scale(0.7);
+  opacity: 0;
+}
+  100%{
+    opacity: 1;
+    visibility: visable;
+    z-index: 0;
+    transform: translateY(0px) scale(0.7);
+    transform: scale(1);
+   
+  }
+
+`;
+
+export const fadeInUp = keyframes`
+  0% {
+    opacity: 0;
+    transform: translate3d(0, 100%, 0);
+  }
+
+  20% {
+    opacity: 1;
+    transform: translate3d(0, 0, 0);
+  }
+  `;
+
+export const rollIn = keyframes`
+  from {
+    opacity: 0;
+    transform: translate3d(-100%, 0, 0) rotate3d(0, 0, 1, -120deg);
+  }
+
+  to {
+    opacity: 1;
+    transform: translate3d(0, 0, 0);
+  }
+
+`;
+
+export const flash = keyframes`
+  0%,
+  40%,
+  80% {
+    opacity: 1;
+  }
+
+  20%,
+  60%,
+  100%{
+    opacity: 0;
+  }
+
+`;
+
+export const rotateIn = keyframes`
+  from {
+    transform: rotate3d(0, 0, 1, -200deg);
+    opacity: 0;
+  }
+
+  to {
+    transform: translate3d(0, 0, 0);
+    opacity: 1;
+  }
+  `;
+
+export const fadeOutUp = keyframes` {
+ from {
+    opacity: 1;
+  }
+
+  to {
+    opacity: 1;
+    transform: translate3d(0, -100%, 0);
+  }
+  `;
+
+export const fadeOutUp2 = keyframes` {
+ from {
+    opacity: 1;
+  }
+
+  to {
+    opacity: 0;
+    z-index:-1;
+    transform: translate3d(0, -100%, 0);
+  }
+  `;
+
+export const rotateInFadeOutUp = keyframes`
+  0% {
+    /* z-index:1; */
+    transform: rotate3d(0, 0, 1, -200deg);
+    opacity: 0;
+  }
+
+  30% {
+    transform: translate3d(0, 0, 0);
+    opacity: 1;
+  }
+  100% {
+    /* opacity: 0; */
+    transform: translate3d(0, -100%, 0);
+  }
+  `;
+export const slideLeft = keyframes`
+  from {
+   opacity:0;
+
+  }
+  to {
+    opacity:1;
+
+
+  }
+`;
+
+export const rotateOut = keyframes`
+  from {
+    opacity: 1;
+    z-index:0;
+  }
+
+  70% {
+    transform: rotate3d(0, 0, 1, 200deg);
+   
+
+  }
+  100%{
+    opacity: 0;
+    z-index:-1;
+  }
+`;
+
+export const fadeIn = keyframes`
+   0% {
+      opacity 0;
+      z-index: 0;
+      }
+   100% {
+    opacity 1;
+    z-index: 0;
+    
+   }
+   `;
