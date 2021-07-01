3588ac57a532080684de15f1661890ee306c0981
DoubleHelix <tjrnyc5@gmail.com>
2021-07-01 16:42:21 -0400

    added error checks and more animations

--- matic-mumbai-test-client/src/shared/_Keyframes.js
+++ matic-mumbai-test-client/src/shared/_Keyframes.js
@@ -653,3 +653,25 @@ from {
     transform: translate3d(0, 0, 0);
   }
 `;
+
+export const lightSpeedInLeft = keyframes`
+  from {
+    transform: translate3d(-100%, 0, 0) skewX(30deg);
+    opacity: 0;
+  }
+
+  60% {
+    transform: skewX(-20deg);
+    opacity: 1;
+  }
+
+  80% {
+    transform: skewX(5deg);
+  }
+
+  to {
+    transform: translate3d(0, 0, 0);
+    opacity: 1;
+
+  }
+  `;
